a31c3458657fd827ea345de62ee26b6f20756d08
Gloria Friesen and Riley Watts <riley@>
2017-05-02 11:59:41 -0700

    add ingredients and directions to Recipe class

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
     <img src="resources/images/bobs.jpeg">
     <h2>Burgers</h2>
     <ul>
-      <li *ngFor="let recipe of recipes">{{recipe.title}}</li>
+      <li *ngFor="let recipe of recipes">{{recipe.title}} {{recipe.ingredients}} {{recipe.directions}}</li>
     </ul>
   </div>
   `
@@ -27,13 +27,13 @@ export class AppComponent {
   minute: number = this.currentTime.getMinutes();
   time: string = this.hour + ':' + this.minute;
   recipes: Recipe[] = [
-    new Recipe('Smells Like Bean Spirit'),
-    new Recipe('Poutine on the Ritz Burger'),
-    new Recipe('Cheeses is Born Burger'),
-    new Recipe('Don\'t you Four Chedda\' \'Bout Me Burger')
+    new Recipe('Smells Like Bean Spirit', ['beans', 'pepper jack'], ['1. Re-fry them beans', '2. Add pepper jack cheese']),
+    new Recipe('Poutine on the Ritz Burger', ['poutine', 'gravy', 'cheese curds'], ['1. Cook up some poutine', '2. Throw in some gravy', '3. Simmer...']),
+    new Recipe('Cheeses is Born Burger', ['mozzarella'], ['1. Stick the cheese in the burger', '2. Cook burger'] ),
+    new Recipe('Don\'t you Four Chedda\' \'Bout Me Burger', ['cheddar', 'white cheddar', 'smoked cheddar', 'extra sharp cheddar'], ['1. Create a 4 cheese blend of your 4 cheeses', '2. Place the mixture on top of your burger'])
   ];
 }
 
 export class Recipe {
-  constructor(public title: string) {}
+  constructor(public title: string, public ingredients: string[], public directions: string[]) {}
 }
